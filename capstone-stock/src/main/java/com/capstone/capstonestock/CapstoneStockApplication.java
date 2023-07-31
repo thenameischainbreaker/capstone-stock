8f08ade52de2e68bd2b0cfe9d1c3a83e44eb02ad
Jasper Javillo <jasperjavillo@gmail.com>
2023-07-31 11:18:07 -0400

    Created capstone-stock project

---
+++ capstone-stock/src/main/java/com/capstone/capstonestock/CapstoneStockApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.capstonestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneStockApplication.class, args);
+	}
+
+}
---
+++ capstone-stock/src/test/java/com/capstone/capstonestock/CapstoneStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.capstonestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
